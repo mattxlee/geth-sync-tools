9a94d698735721d37cbe6aa1a4e4ac1a572ee5cf
Matthew Lee <mattxlee@gmail.com>
2024-07-21 08:15:47 +0000

    Change the script for adding new nodes.
    Use geth command without docker container.

--- add_peers.sh
+++ add_peers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #read first argument, should be file with enode addresses
-if [ -z "$1" ]; then 
+if [ -z "$1" ]; then
     filename='nodes.txt' #default value
 else
     filename=$1
@@ -9,7 +9,7 @@ fi
 
 #read second argument, should be URL to node http endpoint
 if [ -z "$2" ]; then
-    node_URL='http://sandbox:8545' #default value
+    node_URL='http://localhost:3334' #default value
 else
     node_URL=$2
 fi
@@ -19,10 +19,10 @@ echo "Reading from $filename nodes and add this to $node_URL..."
 #Adding node address from list as a peer to node:
 counter=0 #counter for succeed added peers
 while read PEER
-do  
+do
     echo $PEER
-    if 
-        docker run --rm ethereum/client-go --exec "admin.addPeer('$PEER')" attach $node_URL ; 
+    if
+        geth --authrpc.jwtsecret /tmp/jwtsecret --exec "admin.addPeer('$PEER')" attach $node_URL ;
     then
         counter=$((counter+1)) ; echo "node #$counter added"
     else
